b5079cd63b245986134e730ad2e7ef25e8507338
Bevan <poissonpie@gmail.com>
2011-07-07 19:19:38 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,97 @@
+import os
+import random
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+from google.appengine.api import users
+from google.appengine.ext import db
+from google.appengine.api import images
+
+
+class Guitars(db.Model):
+    name = db.StringProperty()
+    rand = db.FloatProperty()
+    image = db.BlobProperty()
+
+class GetGuitarImage(webapp.RequestHandler):
+  def get(self, awidth=None, aheight=None):
+    rand_number = random.random()
+    guitar_to_display = Guitars.all().order('rand').filter('rand >=', rand_number).get()
+    if guitar_to_display is None:
+      guitar_to_display = Guitars.all().order('rand').get()
+    
+    an_image = images.Image(guitar_to_display.image)
+    
+    #get our width and height - set defaults from the actual image
+    # if for some reason the arguments passed in are not integers
+    try:
+      required_width = int(awidth)
+    except:
+      required_width = an_image.width
+    try:
+      required_height = int(aheight)
+    except:
+      required_height = an_image.height
+    
+    #work out our aspect ratios
+    current_aspect_ratio = float(an_image.width) / float(an_image.height)
+    required_aspect_ratio = float(required_width) / float(required_height)
+    if current_aspect_ratio > required_aspect_ratio:
+      #resize height, crop width
+      an_image.resize(height = required_height)
+      an_image.execute_transforms()
+      trim_ratio = float(required_width) / float(an_image.width)
+      x_left = (1.0 - trim_ratio) / 2.0
+      an_image.crop(x_left, 0.0, x_left + trim_ratio, 1.0)
+      final_image = an_image.execute_transforms()
+    elif current_aspect_ratio < required_aspect_ratio:
+      #resize width, crop height
+      an_image.resize(width = required_width)
+      an_image.execute_transforms()
+      trim_ratio = float(required_height) / float(an_image.height)
+      y_top = (1.0 - trim_ratio) / 2.0
+      an_image.crop( 0.0, y_top, 1.0, y_top + trim_ratio)
+      final_image = an_image.execute_transforms()
+    else:
+      an_image.resize(width = required_width, height = required_height)
+      final_image = an_image.execute_transforms()
+    self.response.headers['Content-Type'] = "image/jpeg"
+    self.response.out.write(final_image)
+
+class AddGuitar(webapp.RequestHandler):
+    def get(self):
+        if users.get_current_user():
+            path = os.path.join(os.path.dirname(__file__), 'templates/addguitar.html')
+            self.response.out.write(template.render(path, ""))
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+    def post(self):
+        if users.get_current_user():
+            guitar = Guitars()
+            #first, the easy request items
+            guitar.name = self.request.get('guitarname')
+            #now the images
+            theimage = self.request.get("guitarimage")
+            guitar.image = db.Blob(theimage)
+            #our random number so we can get a random guitar
+            guitar.rand = random.random() 
+            guitar.put()
+            self.redirect('/add-guitar')
+        else:
+            self.redirect('/')
+
+class Index(webapp.RequestHandler):
+    def get(self, trailingurl = None):
+      path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
+      self.response.out.write(template.render(path, ""))
+
+application = webapp.WSGIApplication(
+                                     [('/add-guitar', AddGuitar),
+                                      (r'/(\d+)/(\d+)/?', GetGuitarImage),
+                                      ('/.*', Index),],
+                                     debug=True)
+def main():
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
